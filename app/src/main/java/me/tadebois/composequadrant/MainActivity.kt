27a1bc46cf8f0434ea96c857db343ffbcb805814
Jean Tadebois <jpm.tadebois@gmail.com>
2023-07-03 12:31:26 +1000

    Add Quadrant composable

--- app/src/main/java/me/tadebois/composequadrant/MainActivity.kt
+++ app/src/main/java/me/tadebois/composequadrant/MainActivity.kt
@@ -3,13 +3,23 @@ package me.tadebois.composequadrant
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import me.tadebois.composequadrant.ui.theme.ComposeQuadrantTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +32,11 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Quadrant(
+                        title = stringResource(R.string.text_composable_title),
+                        body = stringResource(R.string.text_composable_body),
+                        colorResource(id = R.color.quadrant_one)
+                    )
                 }
             }
         }
@@ -30,17 +44,37 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun Quadrant(title: String, body: String, color: Color, modifier: Modifier = Modifier) {
+    Surface(
+        color = color,
         modifier = modifier
-    )
+    ) {
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.padding(16.dp)
+        ) {
+            Text(
+                text = title,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.padding(bottom = 16.dp)
+            )
+            Text(
+                text = body,
+                textAlign = TextAlign.Justify
+            )
+        }
+    }
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
-fun GreetingPreview() {
+fun QuadrantPreview() {
     ComposeQuadrantTheme {
-        Greeting("Android")
+        Quadrant(
+            title = stringResource(R.string.text_composable_title),
+            body = stringResource(R.string.text_composable_body),
+            colorResource(id = R.color.quadrant_one)
+        )
     }
 }
